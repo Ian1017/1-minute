02792cad8b8718690d47506b9f56d7b0ab9294fc
ian1017 <mdawidamengo@gmail.com>
2020-01-16 15:02:25 +0300

    Create pitches file

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ login_manager.login_view = 'auth.login'
 
 
 
-def create_app(config_name);
+def create_app(config_name):
     app = Flask(__name__)
 
     
@@ -36,7 +36,7 @@ def create_app(config_name);
     #from .main import main as main_blueprint
     #app.register_blueprint(main_blueprint)
 
-    from .auth import as auth_blueprint
+    from .auth import auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     #configure UploadSet
