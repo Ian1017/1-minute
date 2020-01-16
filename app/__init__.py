31c9ef3b1cd72a01b343e5a697f43bcafabe58a4
ian1017 <mdawidamengo@gmail.com>
2020-01-16 12:39:12 +0300

    declare app functionality

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -10,9 +12,12 @@ db = SQLAlchemy()
 def create_app(config_name);
     app = Flask(__name__)
 
+    
     #Creating the app configurations
     app.config.from_object(config_options[config_name])
 
     #Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+
+    return app
