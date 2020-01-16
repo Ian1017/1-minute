bf18a1fa64b8264f565e2f94f1114c4cb66840bc
ian1017 <mdawidamengo@gmail.com>
2020-01-16 10:11:01 +0300

    add a class production configuration

--- config.py
+++ config.py
@@ -4,4 +4,43 @@ class Config:
     '''
     General configuration parent
     '''
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ian:diamonds@localhost/P'
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration with general configuration class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ian:diamonds@localhost/P'
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General Configuration Settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ian:diamonds@localhost/P'
+
+
+    DEBUG = True
+    ENV = 'development'
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
